1f3cb2f79fa2ca3f344768f82f112193c9ad1549
Gianluca Della Vedova <gianluca@dellavedova.org>
2015-03-18 11:19:23 +0100

    Increase runtime to 30 minutes

--- paper/run_experiment.rb
+++ paper/run_experiment.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 
-[10, 20, 40, 60].each do |n|
+[60].each do |n|
   [2*n/4, 3*n/4, 4*n/4, 5*n/4, 6*n/4].each do |m|
     dirname = "exp/#{n}/#{m}"
     system("mkdir -p #{dirname}") unless Dir.exists?(dirname)
@@ -11,7 +11,7 @@ require 'fileutils'
         system "touch #{dirname}/#{i}.ms"
         system "./ms #{n} 1 -s #{m} -r #{4 * 0.025 * n * m} #{m}  | tail -n +7 | sort | uniq -c > #{dirname}/#{i}.ms"
         system "./ms2ppp #{dirname}/#{i}.ms > #{dirname}/#{i}.data"
-        system "/usr/bin/time -f \"%e\" -o #{dirname}/#{i}.log timeout -s 9 10m ./cppp  #{dirname}/#{i}.data > #{dirname}/#{i}.out"
+        system "/usr/bin/time -f \"%e\" -o #{dirname}/#{i}.log timeout -s 9 30m ./cppp  #{dirname}/#{i}.data > #{dirname}/#{i}.out"
       end
       [1, 2, 4, 8, 16].each do |r|
         mod_dirname = "mod/#{n}/#{m}/#{i}/#{r}"
@@ -21,7 +21,7 @@ require 'fileutils'
             unless File.exists?("#{mod_dirname}/#{x}.data")
               system "touch #{mod_dirname}/#{x}.data"
               system "./random-ppp.rb -r #{r} -m #{dirname}/#{i}.data > #{mod_dirname}/#{x}.data"
-              system "/usr/bin/time -f \"%e\" -o #{mod_dirname}/#{x}.log timeout -s 9 10m ./cppp  #{mod_dirname}/#{x}.data > #{mod_dirname}/#{x}.out"
+              system "/usr/bin/time -f \"%e\" -o #{mod_dirname}/#{x}.log timeout -s 9 30m ./cppp  #{mod_dirname}/#{x}.data > #{mod_dirname}/#{x}.out"
             end
           end
         end
